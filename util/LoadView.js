078d5cf10b6c441101e5292aff62af9e60e55718
姜小鱼 <1847535232@qq.com>
2024-02-15 23:38:57 +0800

    first commit

---
+++ util/LoadView.js
@@ -0,0 +1,37 @@
+
+function isLogin() {
+    return localStorage.getItem("token")
+}
+
+function renderTopbar(user) {
+    // console.log(user)
+    let photo = document.querySelector("#topbar-photo");
+    let currentUsername = document.querySelector("#currentUsername");
+    let exit = document.querySelector("#exit");
+
+    photo.src = user.photo;
+    currentUsername.innerHTML = user.username;
+
+    exit.onclick = () => {
+        // 清理token
+        localStorage.removeItem("token");
+        location.href = "/admin/views/login/index.html";
+    }
+}
+
+async function load() {
+    let user = isLogin();
+    if (user) {
+        let topbarText = await fetch("/admin/components/topbar/index.html").then(res => res.text())
+
+        document.querySelector('.topbar').innerHTML = topbarText;
+
+        renderTopbar(JSON.parse(user))
+    } else {
+        location.href = "/admin/views/login/index.html"
+    }
+}
+
+export { load }
+
+
---
+++ views/home/index.js
@@ -0,0 +1,5 @@
+// 引入模块
+
+import { load } from "/admin/util/LoadView.js"
+
+load()  // 加载topbar  //sldemenu
