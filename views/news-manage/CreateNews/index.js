1932bb966b2c056f74b0ba0a0a2e321662007ece
姜小鱼 <1847535232@qq.com>
2024-02-16 15:20:06 +0800

    fest: 首页完成

---
+++ views/news-manage/CreateNews/index.js
@@ -0,0 +1,6 @@
+// 引入模块
+
+import { load } from "/admin/util/LoadView.js"
+
+load("sidemenu-addNews")  // 加载topbar  //sldemenu
+
---
+++ views/news-manage/NewsList/index.js
@@ -0,0 +1,6 @@
+// 引入模块
+
+import { load } from "/admin/util/LoadView.js"
+
+load("sidemenu-newsList")  // 加载topbar  //sldemenu
+
